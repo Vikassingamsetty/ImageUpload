8e70b55482687573b82466317b8016b9a3bd7110
Vikas Singamsetty <47664415+Vikassingamsetty@users.noreply.github.com>
2020-04-21 17:02:09 +0530

    Add files via upload

---
+++ image picker/image picker/ViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  image picker
+//
+//  Created by Srans022019 on 27/12/19.
+//  Copyright © 2019 Srans02. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var chooseBuuton: UIButton!
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.imageView.layer.cornerRadius = 50
+        self.imageView.layer.borderWidth = 1
+        self.imageView.layer.borderColor = UIColor.lightGray.cgColor
+        
+        self.chooseBuuton.layer.cornerRadius = 5
+    }
+
+    
+    @IBAction func btnClicked() {
+        
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { _ in
+            self.openCamera()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Choose Photo", style: .default, handler: { _ in
+            self.openGallary()
+        }))
+        
+        alert.addAction(UIAlertAction.init(title: "Cancel", style: .destructive, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK: - Open the camera
+    func openCamera(){
+        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerController.SourceType.camera)){
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            //If you dont want to edit the photo then you can set allowsEditing to false
+            imagePicker.allowsEditing = true
+            imagePicker.delegate = self
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        else{
+            let alert  = UIAlertController(title: "Warning", message: "You don't have camera", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    //MARK: - Choose image from camera roll
+    
+    func openGallary(){
+        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        //If you dont want to edit the photo then you can set allowsEditing to false
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+}
+
+//MARK: - UIImagePickerControllerDelegate
+
+extension ViewController:  UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        /*
+         Get the image from the info dictionary.
+         If no need to edit the photo, use `UIImagePickerControllerOriginalImage`
+         instead of `UIImagePickerControllerEditedImage`
+         */
+        if let editedImage = info[.originalImage] as? UIImage {
+            
+            self.imageView.image = editedImage
+            print("Image picked")
+        
+        }
+        
+        //Dismiss the UIImagePicker after selection
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.isNavigationBarHidden = false
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+
+
